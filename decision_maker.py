5303f0670306b51dcf375e3a570c084a3622ac1c
hillarynve <hillaryan@g.ucla.edu>
2024-07-10 13:24:31 -0700

    updated to basic outfit application in terminal

--- decision_maker.py
+++ decision_maker.py
@@ -4,17 +4,40 @@ def get_random_option(options):
     return random.choice(options)
 
 def main():
-    options = ["red", "orange", "yellow", "green", "blue", "purple", "orange"]
+    options = {'top': ["red top", "blue top", "orange shirt", "beige cardigan", "collared shirt"],
+               'bottom': ["yellow pants", "purple skirt", "jeans", "pleated skirt", "tiered skit", "gym shorts"],
+               'shoes': ["white shoes", "black boots"],
+               'fullbody': ["orange dress", "red dress", "jumpsuit"]
+               }
 
-    print("Available options:", ", ".join(options))
+    # Initial prompt
+    print("Are you trying to pick an outfit today? (y/n)")
+    pick_outfit_today = input().strip().lower()
+    
+    if pick_outfit_today != 'y':
+        print("Have a nice day!")
+        return
+    
+    while True:
+        outfit_choice = input("Do you want a two-piece or one-piece outfit?(one/two): ").strip().lower()
+    
+        if outfit_choice == 'one':
+            # randomly choose a top, bottom, and shoes
+            top_choice = get_random_option(options['top'])
+            bottom_choice = get_random_option(options['bottom'])
+            shoes_choice = get_random_option(options['shoes'])
+            print(f"Today's outfit is: the {top_choice} and {bottom_choice} paired with the {shoes_choice}")
+            break
+        elif outfit_choice == 'two':
+            # randomly choose a fullbody outfit
+            fullbody_choice = get_random_option(options['fullbody'])
+            shoes_choice = get_random_option(options['shoes'])
+            print(f"Today's outfit is: the {fullbody_choice} paired with the {shoes_choice}")
+            break
+        else:
+            print("Please enter 'one' or 'two.")
 
-    user_input = input("Do you want to pick a random color? (yes/no): ").strip().lower()
-
-    if user_input == 'yes':
-        random_choice = get_random_option(options)
-        print(f"Randomly chosen color: {random_choice}")
-    else:
-        print("No color was chosen. ")
+    print("Enjoy today's outfit!")
 
 if __name__ == "__main__":
     main()
