a3618bef2ae20dae5be1dd61bf922c0d1ebf988f
hillarynve <hillaryan@g.ucla.edu>
2024-07-16 17:28:19 -0700

    added search by name

--- app.py
+++ app.py
@@ -24,17 +24,24 @@ def menu():
             clothing_type = input("Enter the clothing item's type: ")
             color_pattern = input("Enter the clothing item's color: ")
             fashion_type = input("Enter the clothing item's fashion style: ")
-
             database.add_clothes(connection, name, body_location, clothing_type, color_pattern, fashion_type)
         elif user_input == "2":
             clothing = database.get_all_clothes(connection)
 
+            print("The wardrobe contains: ")
             for item in clothing:
-                print(item)
+                print(f"{item[1]} in a {item[5]} style.")
         elif user_input == "3":
-            pass
+            name = input("Enter the clothing item's name to find: ")
+            clothing = database.get_clothes_by_name(connection, name)
+
+            for item in clothing:
+                print(f"{name} in {fashion_type} style")
         elif user_input == "4":
-            pass
+            name = input("Enter the clothing item's name to find: ")
+            color_option = database.get_color_pattern_for_clothes(connection, name)
+
+            print(f"The color or pattern of the {name} is {color_option[0]}")
         elif user_input == "5":
             name = input("Enter the name of the clothing item to delete: ")
             database.delete_clothing_by_name(connection, name)
